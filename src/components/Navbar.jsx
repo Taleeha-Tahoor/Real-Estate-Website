70347a5e4e734b8d30ab4282d3944b54f2497296
Taleeha Tahoor <taleeha789@gmail.com>
2025-05-16 17:13:47 +0500

    Make Navbar responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "../styles/Navbar.css"
 
 const Navbar = () => {
+  const [menu, setMenu] = useState(false)
+
   return (
     <div className='main'>
       <h3>UrbanNest</h3>
 
-      <div className='inner'>
+      <div className={`inner ${menu ? "show" : ""}`}>
         <a href="#">Home</a>
         <a href="#">About Us</a>
         <a href="#">Property</a>
@@ -14,7 +16,12 @@ const Navbar = () => {
         
       </div>
 
-      <a href='#' className='btn'>Find A House</a>
+      <a href='#' className={`btn ${menu ? "show" : ""}`}>Find A House</a>
+      <i className='material-icons menu-icon' onClick={() => { setMenu(!menu)
+        console.log(menu)
+      }}>
+        menu
+        </i>
     </div>
   )
 }
