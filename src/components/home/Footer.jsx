e873319f20c826b1f23cdcb8a070ebfd48c18f35
Taleeha Tahoor <taleeha789@gmail.com>
2025-06-18 21:50:35 +0500

    Made improvements

--- src/components/home/Footer.jsx
+++ src/components/home/Footer.jsx
@@ -4,7 +4,15 @@ import "../../styles/home/Footer.css"
 const Footer = () => {
   return (
     <div className='footer_main'>
-      
+      <div className='founder_details'>
+        <h4>Kevin Durant</h4>
+        <p>Founder, La Maison</p>
+      </div>
+
+      <p className='founder_thoughts'>“Real estate is real property that consists of land and improvements, 
+        which include buildings, fixtures, roads, structures, and utility systems.
+        Property rights give a title of ownership to the land, improvements,
+        and natural resources such as minerals, plants, animals, water, etc.”</p>
     </div>
   )
 }
